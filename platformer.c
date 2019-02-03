f2d39cc3091dc9b4f04f9022b4e8e1cc5daa8e2e
Peter Rust <peter@cornerstonenw.com>
2019-02-03 12:58:52 -0800

    Include SDL .h files under source control

--- platformer.c
+++ platformer.c
@@ -5,10 +5,10 @@
 #include <math.h>
 #include <limits.h>
 
-#include "SDL.h"
-#include "SDL_image.h"
-#include "SDL_mixer.h"
-#include "font8x8_basic.h"
+#include "include/SDL.h"
+#include "include/SDL_image.h"
+#include "include/SDL_mixer.h"
+#include "include/font8x8_basic.h"
 
 typedef unsigned char byte;
 
